a88ee034bcf2a6686c4f7e69778ca69fb784237d
soyeonnn <no02031@naver.com>
2022-11-14 17:11:39 +0900

    ⚡️ decode 코드 추가

--- src/main/java/com/munjigoorm/backend/main/controller/MainController.java
+++ src/main/java/com/munjigoorm/backend/main/controller/MainController.java
@@ -4,6 +4,9 @@ import com.munjigoorm.backend.main.service.MainService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 @RestController
 @RequestMapping("/main")
 public class MainController {
@@ -12,12 +15,15 @@ public class MainController {
     private MainService mainService;
 
     @GetMapping
-    public String airInfo(@RequestParam String stationName, @RequestParam String addr) {
-        return mainService.getAirInfo(stationName, addr);
+    public String airInfo(@RequestParam String stationName, @RequestParam String addr) throws UnsupportedEncodingException {
+        String station = URLDecoder.decode(stationName, "UTF-8");
+        String address = URLDecoder.decode(addr, "UTF-8");
+        return mainService.getAirInfo(station, address);
     }
 
     @GetMapping(value = "/search")
-    public String airInfo(@RequestParam String keyword) {
-        return mainService.getRegionList(keyword);
+    public String airInfo(@RequestParam String keyword) throws UnsupportedEncodingException {
+        String searchKeyword = URLDecoder.decode(keyword, "UTF-8");
+        return mainService.getRegionList(searchKeyword);
     }
 }
