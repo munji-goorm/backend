8f171b88467c8f56c292068e35fb9bf912b10a86
soyeonnn <no02031@naver.com>
2022-11-17 17:41:58 +0900

    rt_nationwide_info entity 삭제

--- src/main/java/com/munjigoorm/backend/main/repository/CityStationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.munjigoorm.backend.main.repository;
-
-import com.munjigoorm.backend.main.entity.CityStation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CityStationRepository extends JpaRepository<CityStation, String> {
-}
