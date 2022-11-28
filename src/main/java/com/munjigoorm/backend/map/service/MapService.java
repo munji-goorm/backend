1935387579c50a357f85dd670ee12bea3b6d1320
soyeonnn <no02031@naver.com>
2022-11-28 20:22:38 +0900

    ✨ ver2 map 지도 level에 따른 마커 제공 api

--- src/main/java/com/munjigoorm/backend/map/service/MapService.java
+++ src/main/java/com/munjigoorm/backend/map/service/MapService.java
@@ -37,33 +37,115 @@ public class MapService {
         responseJson.addProperty("status", 200);
         responseJson.addProperty("success", true);
 
-        List<Station> stations = null;
+        List<Station> stations = new ArrayList<>();
         List<MapResponse> mapResponses = new ArrayList<>();
 
         // mapLevel에 따라서 분할하는지 확인
-        if(mapLevel >= 1 && mapLevel <= 6) {
+        if(mapLevel >= 1 && mapLevel <= 8) {
             // 요청한 위도, 경도 안에 있는 측정소의 x좌표, y좌표 모두 가져오기
             stations = stationRepository.findByDmXBetweenAndDmYBetween(xOne, xTwo, yOne, yTwo);
-            mapResponses = getStationInfo(stations, mapResponses, "all");
-        } else if(mapLevel >= 7 && mapLevel <= 10) {
-            // 총 9구역으로 나누고, 한 구역별로 30개씩 가져오기
+            mapResponses = getStationInfo(stations, mapResponses, "all", 0);
+        } else if(mapLevel == 9) {
+            // 총 4구역으로 나누고, 한 구역별로 10개씩 가져오기
+            double xDiff = (xTwo - xOne) / 2.0;
+            double yDiff = (yTwo - yOne) / 2.0;
+
+            stations = stationRepository.findByDmXBetweenAndDmYBetween(xOne, xOne+xDiff, yOne, yOne+yDiff);
+            mapResponses = getStationInfo(stations, mapResponses, "limit", 10);
+            stations = stationRepository.findByDmXBetweenAndDmYBetween(xOne+xDiff, xTwo, yOne, yOne+yDiff);
+            mapResponses = getStationInfo(stations, mapResponses, "limit", 10);
+            stations = stationRepository.findByDmXBetweenAndDmYBetween(xOne, xOne+xDiff, yOne+yDiff, yTwo);
+            mapResponses = getStationInfo(stations, mapResponses, "limit", 10);
+            stations = stationRepository.findByDmXBetweenAndDmYBetween(xOne+xDiff, xTwo, yOne+yDiff, yTwo);
+            mapResponses = getStationInfo(stations, mapResponses, "limit", 10);
+        } else if(mapLevel == 10) {
+            // 총 6구역으로 나누고, 한 구역별로 7개씩 가져오기
             double xDiff = (xTwo - xOne) / 3.0;
-            double yDiff = (yTwo - yOne) / 3.0;
+            double yDiff = (yTwo - yOne) / 2.0;
 
+            stations = stationRepository.findByDmXBetweenAndDmYBetween(xOne, xOne+xDiff, yOne, yOne+yDiff);
+            mapResponses = getStationInfo(stations, mapResponses, "limit", 7);
+            stations = stationRepository.findByDmXBetweenAndDmYBetween(xOne+xDiff, xOne+xDiff*2, yOne, yOne+yDiff);
+            mapResponses = getStationInfo(stations, mapResponses, "limit", 7);
+            stations = stationRepository.findByDmXBetweenAndDmYBetween(xOne+xDiff*2, xTwo, yOne, yOne+yDiff);
+            mapResponses = getStationInfo(stations, mapResponses, "limit", 7);
 
+            stations = stationRepository.findByDmXBetweenAndDmYBetween(xOne, xOne+xDiff, yOne+yDiff, yTwo);
+            mapResponses = getStationInfo(stations, mapResponses, "limit", 7);
+            stations = stationRepository.findByDmXBetweenAndDmYBetween(xOne+xDiff, xOne+xDiff*2, yOne+yDiff, yTwo);
+            mapResponses = getStationInfo(stations, mapResponses, "limit", 7);
+            stations = stationRepository.findByDmXBetweenAndDmYBetween(xOne+xDiff*2, xTwo, yOne+yDiff, yTwo);
+            mapResponses = getStationInfo(stations, mapResponses, "limit", 7);
+        } else if(mapLevel == 11) {
+            // 총 8구역으로 나누고, 한 구역별로 5개씩 가져오기
+            double xDiff = (xTwo - xOne) / 4.0;
+            double yDiff = (yTwo - yOne) / 2.0;
+
+            stations = stationRepository.findByDmXBetweenAndDmYBetween(xOne, xOne+xDiff, yOne, yOne+yDiff);
+            mapResponses = getStationInfo(stations, mapResponses, "limit", 5);
+            stations = stationRepository.findByDmXBetweenAndDmYBetween(xOne+xDiff, xOne+xDiff*2, yOne, yOne+yDiff);
+            mapResponses = getStationInfo(stations, mapResponses, "limit", 5);
+            stations = stationRepository.findByDmXBetweenAndDmYBetween(xOne+xDiff*2, xOne+xDiff*3, yOne, yOne+yDiff);
+            mapResponses = getStationInfo(stations, mapResponses, "limit", 5);
+            stations = stationRepository.findByDmXBetweenAndDmYBetween(xOne+xDiff*3, xTwo, yOne, yOne+yDiff);
+            mapResponses = getStationInfo(stations, mapResponses, "limit", 5);
+
+            stations = stationRepository.findByDmXBetweenAndDmYBetween(xOne, xOne+xDiff, yOne+yDiff, yTwo);
+            mapResponses = getStationInfo(stations, mapResponses, "limit", 5);
+            stations = stationRepository.findByDmXBetweenAndDmYBetween(xOne+xDiff, xOne+xDiff*2, yOne+yDiff, yTwo);
+            mapResponses = getStationInfo(stations, mapResponses, "limit", 5);
+            stations = stationRepository.findByDmXBetweenAndDmYBetween(xOne+xDiff*2, xOne+xDiff*3, yOne+yDiff, yTwo);
+            mapResponses = getStationInfo(stations, mapResponses, "limit", 5);
+            stations = stationRepository.findByDmXBetweenAndDmYBetween(xOne+xDiff*3, xTwo, yOne+yDiff, yTwo);
+            mapResponses = getStationInfo(stations, mapResponses, "limit", 5);
         } else {
+            // 시도별로 한개씩 표시
+            Optional<Station> seoul = stationRepository.findById("중구");
+            if(seoul.isPresent()) stations.add(seoul.get());
+            Optional<Station> ulsan = stationRepository.findById("대송동");
+            if(ulsan.isPresent()) stations.add(ulsan.get());
+            Optional<Station> chungbuk = stationRepository.findById("가덕면");
+            if(chungbuk.isPresent()) stations.add(chungbuk.get());
+            Optional<Station> jeonbuk = stationRepository.findById("계화면");
+            if(jeonbuk.isPresent()) stations.add(jeonbuk.get());
+            Optional<Station> gyeonggi = stationRepository.findById("가남읍");
+            if(gyeonggi.isPresent()) stations.add(gyeonggi.get());
+            Optional<Station> chungnam = stationRepository.findById("공주");
+            if(chungnam.isPresent()) stations.add(chungnam.get());
+            Optional<Station> busan = stationRepository.findById("개금동");
+            if(busan.isPresent()) stations.add(busan.get());
+            Optional<Station> gangwon = stationRepository.findById("갈말읍");
+            if(gangwon.isPresent()) stations.add(gangwon.get());
+            Optional<Station> gyeongbuk = stationRepository.findById("평화남산동");
+            if(gyeongbuk.isPresent()) stations.add(gyeongbuk.get());
+            Optional<Station> daejeon = stationRepository.findById("둔산동");
+            if(daejeon.isPresent()) stations.add(daejeon.get());
+            Optional<Station> sejong = stationRepository.findById("부강면");
+            if(sejong.isPresent()) stations.add(sejong.get());
+            Optional<Station> jeju = stationRepository.findById("강정동");
+            if(jeju.isPresent()) stations.add(jeju.get());
+            Optional<Station> daegu = stationRepository.findById("산격동");
+            if(daegu.isPresent()) stations.add(daegu.get());
+            Optional<Station> incheon = stationRepository.findById("송도");
+            if(incheon.isPresent()) stations.add(incheon.get());
+            Optional<Station> jeonnam = stationRepository.findById("여천동(여수)");
+            if(jeonnam.isPresent()) stations.add(jeonnam.get());
+            Optional<Station> gwangju = stationRepository.findById("서석동");
+            if(gwangju.isPresent()) stations.add(gwangju.get());
+            Optional<Station> gyeongnam = stationRepository.findById("경화동");
+            if(gyeongnam.isPresent()) stations.add(gyeongnam.get());
 
+            mapResponses = getStationInfo(stations, mapResponses, "all", 0);
         }
-
         responseJson.add("data", JsonParser.parseString(gson.toJson(mapResponses)));
         return responseJson.toString();
     }
 
-    public List<MapResponse> getStationInfo(List<Station> stations, List<MapResponse> mapResponses, String type) {
+    public List<MapResponse> getStationInfo(List<Station> stations, List<MapResponse> mapResponses, String type, int limit) {
         int count = 0;
 
         for (Station station : stations) {
-            if(type.equals("limit") && count == 10) break;
+            if(type.equals("limit") && count == limit) break;
 
             // stationName의 대기 오염 정보 조회
             Optional<Air> air = airRepository.findById(station.getStationName());
